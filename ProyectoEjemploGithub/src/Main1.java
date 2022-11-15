c9c4276da45c43966b88c752dcbb3f239ca4f208
Jorge <Jorge@DESKTOP-5FAD0MS>
2022-11-15 20:45:03 +0100

    Se modifica la pregunta

--- ProyectoEjemploGithub/src/Main1.java
+++ ProyectoEjemploGithub/src/Main1.java
@@ -4,7 +4,7 @@ public class Main1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		System.out.println("Hola, a cuanto tienes el medio?");
+		System.out.println("Hola, me dices la hora?");
 	}
 
 }
