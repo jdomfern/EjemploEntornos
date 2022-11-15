285b65044a1b51731f881a8ffc6c8fd43c22da03
Jorge <Jorge@DESKTOP-5FAD0MS>
2022-11-15 20:42:42 +0100

    Añade pregunta
    Se añade pregunta profesional

--- ProyectoEjemploGithub/src/Main1.java
+++ ProyectoEjemploGithub/src/Main1.java
@@ -3,7 +3,8 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Hola");
+	
+		System.out.println("Hola, a cuanto tienes el medio?");
 	}
 
 }
